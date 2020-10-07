a3e88fbbf623022c4f99f69f256b8d12baeb6c1d
Robbie Cook <robbieck7@gmail.com>
2020-10-08 08:20:15 +1300

    Making parser smarter

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # tsdoc-extended
 
 <!-- AUTO-GENERATED-CONTENT:START (API) -->
-* **TsDocExtended**
-	A class to make generating TSDoc markdown easier
+* **index**
+	
+	
+	* **TsDocExtended**
+		A class to make generating TSDoc markdown easier
+	
+		* **generateMarkdown**
+			Generate nice markdown docs with examples
+		
+		Example: `generateMarkdown('src/')`
+	
+		* **getComments**
+			Get comments from an item
+		* **processChildren**
+			Process the children of an item
 <!-- AUTO-GENERATED-CONTENT:END (API) -->
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,29 @@
 import NodeExtended from "node-extended";
 import fs from "fs";
 
+interface Comment {
+  shortText: string;
+  text: string;
+  tags?: Array<{
+    tag: string; // e.g. 'example'
+    text: string;
+  }>;
+}
+
+interface Item {
+  id: number;
+  name: string;
+  kind: number;
+  kindString?: string;
+  flags?: {
+    isExported: true;
+  };
+  originalName?: string;
+  children: Item[];
+  signatures?: Array<Partial<Item>>;
+  comment?: Comment;
+}
+
 // Capitalize a string
 function toCapitalCase(str: string) {
   if (!str || str === "") {
@@ -15,6 +38,89 @@ function toCapitalCase(str: string) {
  * A class to make generating TSDoc markdown easier
  */
 export default class TsDocExtended {
+  /**
+   * Get comments from an item
+   */
+  private static getComments(item: Item | Partial<Item>): Array<string> {
+    let subItems: string[] = [];
+    if (item.comment) {
+      if (item.comment.shortText) {
+        subItems.push(`\t${item.comment.shortText}`);
+      }
+
+      if (item.comment.text) {
+        subItems.push(`${item.comment.text}`);
+      }
+
+      if (item.comment.tags) {
+        subItems.push("");
+        const thing = `${item.comment.tags.map(
+          (tag: { tag: string; text: string }) =>
+            `${toCapitalCase(tag.tag)}: ${tag.text.replace(/\n/g, "\n\t")}`
+        )}`;
+        subItems.push(thing);
+      }
+    }
+    if (item.signatures) {
+      subItems = [...subItems, ...this.getComments(item.signatures[0])];
+    }
+    return subItems;
+  }
+  /**
+   * Process the children of an item
+   */
+  private static processChildren(
+    item: Item,
+    ignoredModules?: Array<string>,
+    depth = 0
+  ): Array<string> | null {
+    if (!item || !item.children) {
+      return null;
+    }
+
+    console.log(`Processing ${item.name}`);
+
+    const lines: string[] = [];
+
+    for (let child of item.children) {
+      if (!child.flags?.isExported) {
+        continue;
+      }
+      console.log(`Processing child ${child.name}`);
+
+      const name = child.name.replace(/"/g, "");
+
+      if (
+        !(
+          ignoredModules &&
+          ignoredModules.includes(child.name.replace(/"/g, "")) &&
+          child.kindString === "Module"
+        ) &&
+        !child.name.includes("Props")
+      ) {
+        lines.push(`* **${name}**`);
+      }
+
+      // Process comments
+
+      lines.push(this.getComments(child).join("\n" + "\t".repeat(depth)));
+
+      // Recursively get data on each child
+      const subChildren = this.processChildren(
+        child,
+        ignoredModules,
+        depth + 1
+      );
+      if (subChildren) {
+        subChildren.unshift("");
+        const subLines = subChildren.join("\n" + "\t".repeat(depth + 1));
+
+        lines.push(subLines);
+      }
+    }
+    return lines;
+  }
+
   /**
    * Generate nice markdown docs with examples
    *
@@ -33,39 +139,12 @@ export default class TsDocExtended {
     console.log("Read JSON index...");
 
     let stringItems: string[] = [];
-    data.children.forEach((item: { name: string; children: any[] }) => {
-      if (!(ignoredModules && item.name.replace(/"/g, "") in ignoredModules)) {
-        item.children?.forEach((child) => {
-          const name = child.name.replace(/"/g, "");
-          if (child.comment && !child.name.includes("Props")) {
-            stringItems.push(`* **${name}**`);
 
-            const subItems = [];
-            if (child.comment.shortText) {
-              subItems.push(`\t${child.comment.shortText}`);
-            }
+    const processedChild = this.processChildren(data);
+    if (processedChild) {
+      stringItems.push(processedChild.join("\n\t"));
+    }
 
-            if (child.comment.text) {
-              subItems.push(`${child.comment.text}`);
-            }
-
-            if (child.comment.tags) {
-              subItems.push("");
-              const item = `${child.comment.tags.map(
-                (tag: { tag: string; text: string }) =>
-                  `${toCapitalCase(tag.tag)}: ${tag.text.replace(
-                    /\n/g,
-                    "\n\t"
-                  )}`
-              )}`;
-              subItems.push(item);
-            }
-
-            stringItems.push(subItems.join("\n\t"));
-          }
-        });
-      }
-    });
     return stringItems.join("\n");
   }
 }
